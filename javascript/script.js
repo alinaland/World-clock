2d6ce1c04ffc486fd603323353a188fd84845fe8
alinaland <114952993+alinaland@users.noreply.github.com>
2022-10-12 14:40:27 -0400

    Added the current location feature, deleted seconds from select menu options

--- javascript/script.js
+++ javascript/script.js
@@ -48,6 +48,9 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -57,9 +60,9 @@ function updateCity(event) {
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format("A")}</small></div>
+          <div class="time">${cityTime.format("h:mm")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
         </div>
         `;
 }
